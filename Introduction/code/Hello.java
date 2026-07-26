96303e8600b6b1bdc1c49a06e2ca844b7168c93e
AbdulShaikz <starabdul100@gmail.com>
2026-07-26 17:38:39 +0530

    Modify Hello.java to prompt user for input and greet by name instead of displaying "Hello World!"

--- Introduction/code/Hello.java
+++ Introduction/code/Hello.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
